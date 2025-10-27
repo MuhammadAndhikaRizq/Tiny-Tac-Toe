1dffc80494e29b8f6d544e77a4909ec7eaf4f010
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-10-27 20:49:52 +0700

    Tile Movement

--- TinyTacToe/Assets/Scripts/Tiles/TileMovement.cs
+++ TinyTacToe/Assets/Scripts/Tiles/TileMovement.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
 
-public class TileMovement
+public class TileMovement: MonoBehaviour
 {
-    
+    [SerializeField] private float range = 1f;
+    [SerializeField] private float speed = 5f;
+
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        targetPosition = startPosition;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+    }
+    private void OnMouseEnter()
+    {
+        targetPosition = startPosition + Vector3.up * range;
+    }
+
+    private void OnMouseExit()
+    {
+        targetPosition = startPosition;
+    }
 }
