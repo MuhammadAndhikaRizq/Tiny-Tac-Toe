fd9e1a68bb85d884883b00539b5d2a0f3c8e0d4c
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-11-04 21:04:48 +0700

    Data Container

---
+++ TinyTacToe/Assets/Scripts/Game Systems/Data/DataContainer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+[System.Serializable]
+public class BoardData
+{
+    public GameObject boardPrefab;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public string boardName;
+}
+public class DataContainer
+{
+    
+}
--- TinyTacToe/Assets/Scripts/Game Systems/Save/InventoryBoard.cs
+++ TinyTacToe/Assets/Scripts/Game Systems/Save/InventoryBoard.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+[System.Serializable]
 
 [CreateAssetMenu(fileName ="Board", menuName = "ScriptableObjects/InventoryBoard")]
 public class InventoryBoard : ScriptableObject
 {
+    [SerializeField]
     public List<GameObject> boardCollections;
 
     public void AddBoard(GameObject board)
